eeb1bb5b4d3c507d319535fb9604a8f40de4e468
ravee <1819318+Raveendran@users.noreply.github.com>
2025-07-08 21:40:49 +1000

    Introduced tags

--- terraform-gcs-module/main.tf
+++ terraform-gcs-module/main.tf
@@ -2,4 +2,9 @@ resource "google_storage_bucket" "bucket" {
   name          = var.bucket_name
   location      = var.bucket_location
   force_destroy = var.force_destroy
+
+  labels = {
+    env = "poc"
+    costcenter = "RK123"
+  }
 }
